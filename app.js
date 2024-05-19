bb04fd1752700368090ea4564c59e9e97ec7d3fa
divyansh1733 <divyanshk072@gmail.com>
2024-05-20 00:18:05 +0530

    added 4 sections

--- app.js
+++ app.js
@@ -1,58 +1,116 @@
-// let clip = document.querySelector('.buds-pro-vid')
-// clip.addEventListener("mouseover",function(e) {
-//     clip.play(e);
-// })
+function loco() {
+  gsap.registerPlugin(ScrollTrigger);
 
-// // clip.addEventListener("mouseout", function (e) { 
-// //     clip.pause(); 
-// // }) 
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-//step 1: get DOM
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
-let timeAutoNext = 7000;
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-nextDom.onclick = function(){
-    showSlider('next');    
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
 }
+loco();
 
-prevDom.onclick = function(){
-    showSlider('prev');    
-}
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    next.click();
-}, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
-    }
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: `#page1`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
 
-    clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
-        next.click();
-    }, timeAutoNext)
-}
+tl.to("#page1>h1", {
+  top: `-50%`,
+});
+
+var tl1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: `#page2`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
+
+tl1.to("#page2>h1", {
+  top: `-50%`,
+});
+
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: `#page4`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
+
+tl2.to("#page4>#center-page4", {
+  top: `-50%`,
+  //   backgroundImage: "transparent",
+  opacity: 0,
+  display: "none",
+  scrollTrigger: {
+    trigger: `#page4>#center-page4`,
+    scroller: `#main`,
+    start: `top -10%`,
+    // markers:true,
+    scrub: 1,
+    // pin:true
+  },
+});
+
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: `#page5`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
+
+tl2.to("#page5>#center-page5", {
+  top: `-50%`,
+  //   backgroundColor: "transparent",
+  opacity: 0,
+  display: "none",
+  scrollTrigger: {
+    trigger: `#page5>#center-page5`,
+    scroller: `#main`,
+    start: `top top`,
+    scrub: 1,
+  },
+});
