d0503b9d099c7683b8adaeb79bf97403e3aea92e
raghavsingh05 <raghavsingh47336@gmail.com>
2024-05-19 20:46:29 +0530

    removed old code

--- app.js
+++
@@ -1,9 +0,0 @@
-let clip = document.querySelector('.buds-pro-vid')
-clip.addEventListener("mouseover",function(e) {
-    clip.play(e);
-})
-
-// clip.addEventListener("mouseout", function (e) { 
-//     clip.pause(); 
-// }) 
-
